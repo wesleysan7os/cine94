e418cee0b02347740560913d5e99fb222764b825
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-01-31 22:36:24 -0300

    displaying posts moc list

--- front-end/src/components/post/Post.jsx
+++ front-end/src/components/post/Post.jsx
@@ -1,8 +1,20 @@
 import "./post.css";
+import postImage from "../../assets/images/pulpfiction.jpg";
 export default function Post() {
   return (
     <div className="post">
-      post
+      <img className="postImg" src={postImage} alt="post image" />
+      <div className="postInfo">
+        <span className="postAuthor">Author: Wesley Santos</span>
+        <span className="postTitle">Pulp Fiction vale a pena?</span>
+        <hr />
+        <div className="postDescription">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sed quisquam
+          voluptas culpa quos, autem illum sequi veniam quia tenetur incidunt? Quos obcaeca
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sed quisquam
+          voluptas culpa quos, autem illum sequi veniam quia tenetur incidunt? Quos obcaeca
+        </div>
+      </div>
     </div>
   );
 }; 
