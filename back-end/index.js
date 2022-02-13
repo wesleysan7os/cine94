61d1c104f1d59cb5e4a1b343dbba54b94cb48db8
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-02-12 23:40:37 -0300

    resolving cors problem

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import blogsRoutes from './routes/blogs.js'
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const PORT = 5000;
 
 app.use(bodyParser.json()); 
